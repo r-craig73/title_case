cecea0b7611fcd6cee0836d009a602ea50462910
Ron Craig <ron.craig@comcast.net>
2018-09-14 21:39:16 -0700

    comment out lines and replace double quotes with single quote

--- spec/title_case_spec.rb
+++ spec/title_case_spec.rb
@@ -2,19 +2,19 @@ require('rspec')
 require('title_case')
 
 describe('#title_case') do
-  it ("return title with capitalized first word") do
-    expect(title_case("apple")).to(eq("Apple"))
+  it('capitalizes the first letter of a word') do
+    expect(title_case('apple')).to(eq('Apple'))
   end
-  it ("capitalizes multiple words") do
-    expect(title_case("apple sauce")).to(eq("Apple Sauce"))
-  end
-  it ("capitalizes multiple words and ignores 'or'") do
-    expect(title_case("apple or sauce")).to(eq("Apple or Sauce"))
-  end
-  it ("capitalizes multiple words and ignores 'or' and 'and'") do
-    expect(title_case("apple or and sauce")).to(eq("Apple or and Sauce"))
-  end
-  it ("capitalizes multiple words and ignores 'or' and 'and' unless it is the first word") do
-    expect(title_case("the apple and the sauce")).to(eq("The Apple and the Sauce"))
-  end
+  # it('capitalizes multiple words') do
+  #   expect(title_case('apple sauce')).to(eq('Apple Sauce'))
+  # end
+  # it('capitalizes multiple words and ignores "or"') do
+  #   expect(title_case('apple or sauce')).to(eq('Apple or Sauce'))
+  # end
+  # it('capitalizes multiple words and ignores "or" & "and"') do
+  #   expect(title_case('apple or and sauce')).to(eq('Apple or and Sauce'))
+  # end
+  # it('ignores "or" & "and" after it is the first word') do
+  #   expect(title_case('the apple and the sauce')).to(eq('The Apple and the Sauce'))
+  # end
 end
