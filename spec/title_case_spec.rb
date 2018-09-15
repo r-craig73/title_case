4e3510e927379c4f0b529347b0e0dd2851faa7a6
Ron Craig <ron.craig@comcast.net>
2018-09-14 23:00:27 -0700

    edit code to include 'and' as the first letter

--- spec/title_case_spec.rb
+++ spec/title_case_spec.rb
@@ -5,6 +5,7 @@ require('title_case')
 describe('#title_case') do
   it('capitalizes the first letter of a word') do
     expect(title_case('purple')).to(eq('Purple'))
+    expect(title_case('and')).to(eq('And'))
   end
   it('capitalizes the first letter of all words in a multiple word title') do
     expect(title_case('the color purple')).to(eq('The Color Purple'))
@@ -12,8 +13,8 @@ describe('#title_case') do
   it('capitalizes multiple words but ignores "or"') do
     expect(title_case('the color purple or green')).to(eq('The Color Purple or Green'))
   end
-  # it('capitalizes multiple words and ignores "or" & "and"') do
-  #   expect(title_case('apple or and sauce')).to(eq('Apple or and Sauce'))
+  # it('capitalizes multiple words but ignores "and"') do
+  #   expect(title_case('the color purple and green')).to(eq('The Color Purple and Green'))
   # end
   # it('ignores "or" & "and" after it is the first word') do
   #   expect(title_case('the apple and the sauce')).to(eq('The Apple and the Sauce'))
