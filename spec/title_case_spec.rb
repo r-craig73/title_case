d8509cb4d0c03e76ff7617b73dd0e27894ca4d97
Ron Craig <ron.craig@comcast.net>
2018-09-14 23:10:34 -0700

    add 4th spec to fail, specs 1-3 are passing

--- spec/title_case_spec.rb
+++ spec/title_case_spec.rb
@@ -13,9 +13,9 @@ describe('#title_case') do
   it('capitalizes multiple words but ignores "or"') do
     expect(title_case('the color purple or green')).to(eq('The Color Purple or Green'))
   end
-  # it('capitalizes multiple words but ignores "and"') do
-  #   expect(title_case('the color purple and green')).to(eq('The Color Purple and Green'))
-  # end
+  it('capitalizes multiple words but ignores "and"') do
+    expect(title_case('the color purple and green')).to(eq('The Color Purple and Green'))
+  end
   # it('ignores "or" & "and" after it is the first word') do
   #   expect(title_case('the apple and the sauce')).to(eq('The Apple and the Sauce'))
   # end
