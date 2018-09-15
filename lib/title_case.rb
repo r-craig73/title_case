cc2ab8211960959caa1f3009063c30eecc9c1e4c
Ron Craig <ron.craig@comcast.net>
2018-09-14 23:01:40 -0700

    add code to consider a user inputting one word as a string

--- lib/title_case.rb
+++ lib/title_case.rb
@@ -2,14 +2,20 @@ require('pry')
 
 def title_case(title)
   split_sentence = title.split
-  split_sentence.each do |word|
-    if word == 'or'
-      word
-    else
-      word.capitalize!
+  if split_sentence.length == 1
+    first_word = split_sentence.shift
+    first_word.capitalize!
+  else
+    split_sentence.each do |word|
+      if word == 'or'
+        word
+      else
+        word.capitalize!
+      end
     end
+    split_sentence.join(' ')
   end
-  split_sentence.join(' ')
+end
   # title_array = title.split(' ')
   # first_word = title_array.shift()
   # capital_first_word = first_word.capitalize!()
@@ -36,4 +42,4 @@ def title_case(title)
   # else
   #   capital_first_word + ' ' + title_array.join(' ')
   # end
-end
+#end
