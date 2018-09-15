f6270d5eaeb6265fce6c929bb6a5e28eda42e805
Ron Craig <ron.craig@comcast.net>
2018-09-15 02:28:10 -0700

    edit specs and place string of words before the title_case method

--- spec/title_case_spec.rb
+++ spec/title_case_spec.rb
@@ -1,33 +1,37 @@
 require('rspec')
-require('pry')
 require('title_case')
 
-describe('#title_case') do
+describe('title_case') do
   it('capitalizes the first letter of a word') do
-    expect(title_case('purple')).to(eq('Purple'))
-    expect(title_case('and')).to(eq('And'))
-    expect(title_case('the')).to(eq('The'))
+    expect('purple'.title_case).to(eq('Purple'))
+    expect('and'.title_case).to(eq('And'))
+    expect('the'.title_case).to(eq('The'))
   end
+
   it('capitalizes the first letter of all words in a multiple word title') do
-    expect(title_case('the color purple')).to(eq('The Color Purple'))
+    expect('the color purple'.title_case).to(eq('The Color Purple'))
   end
+
   it('capitalizes the last letter of all words in a multiple word title') do
-    expect(title_case('the color purple is tooo')).to(eq('The Color Purple Is Tooo'))
+    expect('the color purple is tooo'.title_case).to(eq('The Color Purple Is Tooo'))
   end
+
   it('capitalizes multiple words but ignore English grammar articles') do
-    expect(title_case('the color purple an apple')).to(eq('The Color Purple an Apple'))
-    expect(title_case('the color purple the green')).to(eq('The Color Purple the Green'))
-    expect(title_case('the color purple a bird')).to(eq('The Color Purple a Bird'))
+    expect('the color purple an apple'.title_case).to(eq('The Color Purple an Apple'))
+    expect('the color purple the green'.title_case).to(eq('The Color Purple the Green'))
+    expect('the color purple a bird'.title_case).to(eq('The Color Purple a Bird'))
   end
+
   it('capitalizes multiple words but ignore English grammar coordinating conjunctions') do
-    expect(title_case('the color purple and green')).to(eq('The Color Purple and Green'))
-    expect(title_case('the color purple but blue')).to(eq('The Color Purple but Blue'))
-    expect(title_case('the color purple for mary')).to(eq('The Color Purple for Mary'))
-    expect(title_case('the color purple nor orange')).to(eq('The Color Purple nor Orange'))
+    expect('the color purple and green'.title_case).to(eq('The Color Purple and Green'))
+    expect('the color purple but blue'.title_case).to(eq('The Color Purple but Blue'))
+    expect('the color purple for mary'.title_case).to(eq('The Color Purple for Mary'))
+    expect('the color purple nor orange'.title_case).to(eq('The Color Purple nor Orange'))
   end
+
   it('capitalize multiple words but ignore English grammar prepositions with less then 5 letters') do
-    expect(title_case('the color purple at main')).to(eq('The Color Purple at Main'))
-    expect(title_case('the color purple from mary')).to(eq('The Color Purple from Mary'))
-    expect(title_case('the color purple by joe')).to(eq('The Color Purple by Joe'))
+    expect('the color purple at main'.title_case).to(eq('The Color Purple at Main'))
+    expect('the color purple from mary'.title_case).to(eq('The Color Purple from Mary'))
+    expect('the color purple by joe'.title_case).to(eq('The Color Purple by Joe'))
   end
 end
